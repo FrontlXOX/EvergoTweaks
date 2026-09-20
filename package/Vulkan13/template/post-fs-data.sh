3ca574a31379b344c85a67464eaef074b1e034fa
Shovit Dutta <shovitdutta@proton.me>
2026-09-20 09:24:22 +0530

    🦋 [FEAT]: calibrate Mali AGT frequency to 13MHz, inject EGL format maps & AFBC gralloc configs

--- package/Vulkan13/template/post-fs-data.sh
+++ package/Vulkan13/template/post-fs-data.sh
@@ -6,6 +6,7 @@ chcon -R u:object_r:same_process_hal_file:s0 "$MODDIR/system/vendor/lib" 2>/dev/
 chcon -R u:object_r:same_process_hal_file:s0 "$MODDIR/system/vendor/lib64" 2>/dev/null
 chcon -R u:object_r:same_process_hal_file:s0 "$MODDIR/system/vendor/etc/mali_platform.config" 2>/dev/null
 chcon -R u:object_r:vendor_configs_file:s0 "$MODDIR/system/vendor/etc/permissions" 2>/dev/null
+chcon -R u:object_r:vendor_configs_file:s0 "$MODDIR/system/vendor/etc/gralloc" 2>/dev/null
 
 # Also enforce contexts on live /vendor mountpoints
 chcon -h -R u:object_r:same_process_hal_file:s0 /vendor/lib64/egl 2>/dev/null
@@ -27,4 +28,5 @@ chcon -h -R u:object_r:same_process_hal_file:s0 /vendor/lib/*gpd* 2>/dev/null
 chcon -h -R u:object_r:same_process_hal_file:s0 /vendor/etc/mali_platform.config 2>/dev/null
 chcon -h -R u:object_r:vendor_configs_file:s0 /vendor/etc/permissions/android.hardware.vulkan*.xml 2>/dev/null
 chcon -h -R u:object_r:vendor_configs_file:s0 /vendor/etc/permissions/android.software.vulkan*.xml 2>/dev/null
+chcon -h -R u:object_r:vendor_configs_file:s0 /vendor/etc/gralloc 2>/dev/null
 

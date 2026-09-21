7f73295b413c6d7ce35ae78e513acd790010c02e
Shovit Dutta <shovitdutta@proton.me>
2026-09-21 12:46:38 +0530

    🦑 [CHORE]: update Kaeru flash script

--- src/modules/Kaeru/flash.sh
+++ src/modules/Kaeru/flash.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-
 set -e
-
 echo "=== Flash LK + Enable BLDR Spoof ==="
-fastboot wait-for-device
 fastboot flash lk version-9.bin
-echo "Rebooting to bootloader..."
 fastboot reboot bootloader
-echo "Waiting for bootloader..."
-fastboot wait-for-device
 fastboot oem bldr_spoof on
-echo "Rebooting..."
+fastboot erase userdata
 fastboot reboot
 echo "=== Done ==="
